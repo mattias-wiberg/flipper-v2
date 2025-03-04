b6e91302030ac93d753364758efd4635b06925e2
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-03-04 22:11:29 +0100

    feat: add sign up page

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -27,11 +27,13 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 const formSchema = z.object({
+  nickname: z.string().optional(),
   email: z.string().email(),
   password: z.string(),
+  confirmPassword: z.string(),
 });
 
-export function LoginForm({
+export function SignupForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
@@ -41,8 +43,10 @@ export function LoginForm({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      nickname: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
   });
 
@@ -59,9 +63,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardTitle className="text-xl">Welcome!</CardTitle>
           <CardDescription>
-            Login with your Apple or Google account
+            Sign up with your Apple or Google account
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -70,7 +74,7 @@ export function LoginForm({
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
                   <Button variant="outline" className="w-full">
-                    Login with Apple
+                    Sign up with Apple
                   </Button>
                 </div>
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -86,6 +90,27 @@ export function LoginForm({
                   )}
                   <FormField
                     control={form.control}
+                    name="nickname"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nickname</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Joe The Tank"
+                            type="nickname"
+                            required
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
                     name="email"
                     render={({ field }) => (
                       <FormItem>
@@ -99,7 +124,7 @@ export function LoginForm({
                           />
                         </FormControl>
                         <FormDescription>
-                          This is your public display name.
+                          This is you will use to login with.
                         </FormDescription>
                         <FormMessage />
                       </FormItem>
@@ -111,15 +136,21 @@ export function LoginForm({
                     name="password"
                     render={({ field }) => (
                       <FormItem>
-                        <div className="flex items-center">
-                          <FormLabel>Password</FormLabel>
-                          <a
-                            href="#"
-                            className="ml-auto text-sm underline-offset-4 hover:underline"
-                          >
-                            Forgot your password?
-                          </a>
-                        </div>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input type="password" required {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirm your password</FormLabel>
                         <FormControl>
                           <Input type="password" required {...field} />
                         </FormControl>
@@ -128,13 +159,13 @@ export function LoginForm({
                     )}
                   />
                   <Button type="submit" className="w-full">
-                    Login
+                    Sign up
                   </Button>
                 </div>
                 <div className="text-center text-sm">
-                  Don&apos;t have an account?{" "}
-                  <Link href="/signup" className="underline underline-offset-4">
-                    Sign up
+                  Already have an account?{" "}
+                  <Link href="/login" className="underline underline-offset-4">
+                    Login
                   </Link>
                 </div>
               </div>
