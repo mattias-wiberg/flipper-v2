f29913e60c50a7d613f1fbb67af74069ea37aa20
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-07-27 20:21:01 +0200

    fix: button nesting error

--- components/token.tsx
+++ components/token.tsx
@@ -1,28 +1,21 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { CopyIcon } from "lucide-react";
 
 export const Token = ({ token }: { token: string }) => {
   return (
     <div className="flex gap-2 w-full items-center justify-between">
-      <code className="bg-muted relative rounded my-2 px-[0.3rem] py-[0.3rem] font-mono text-sm font-semibold w-full">
-        {token}
-      </code>
       <Tooltip>
-        <TooltipTrigger>
-          <Button
-            variant="outline"
-            size="icon"
-            className="size-8"
+        <TooltipTrigger className="w-full bg-muted rounded my-2 px-[0.3rem] py-[0.3rem] ">
+          <code
+            className="font-mono text-sm font-semibold"
             onClick={() => navigator.clipboard.writeText(token)}
           >
-            <CopyIcon className="size-4" />
-          </Button>
+            {token}
+          </code>
         </TooltipTrigger>
         <TooltipContent>Click to copy</TooltipContent>
       </Tooltip>
