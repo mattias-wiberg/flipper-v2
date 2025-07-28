20e7c171f519c036bee17f706c69f21b49446e9a
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-07-28 19:38:16 +0200

    refactor: flipper branding

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,4 +1,4 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import { Bot } from "lucide-react";
 
 export default async function Layout({
   children,
@@ -10,9 +10,9 @@ export default async function Layout({
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+            <Bot className="size-4" />
           </div>
-          Acme Inc.
+          Flipper
         </a>
         {children}
       </div>
